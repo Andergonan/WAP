182ac457ba512eae2b607df1e162e28b6e3ea29d
David Anderle <86005883+Andergonan@users.noreply.github.com>
2021-10-09 17:51:50 +0200

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,11 @@
+/*
+Author: David Anderle
+Github: https://github.com/Andergonan/
+LICENSE: https://github.com/Andergonan/WAP/blob/main/LICENSE
+*/
+
+var audio = new Audio("Spooky Scary Skeletons (Remix) Lyrics.mp3"); // https://www.youtube.com/watch?v=pBWLLP9FpTY&ab_channel=DCCLyrics //
+
+document.onclick = function() {
+  audio.play();
+}
